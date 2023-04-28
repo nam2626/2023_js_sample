cacba199a08ea06bf12a06d27771126754dad6ad
nam2626 <nam2626@naver.com>
2023-04-28 17:32:12 +0900

    데이터 타입

---
+++ Step01/data_type.js
@@ -0,0 +1,15 @@
+//데이터 타입
+//1. Number (숫자 - 정수, 실수 포함)
+//2. String (문자열)
+//3. Boolean (논리값 - true, false)
+//4. undefined (값이 할당되지 않은 상태)
+//5. null (값이 없음)
+
+//1. Number
+var a = 10;
+var b = 10.5;
+console.log(typeof a,typeof b);
+//2. String
+var str1 = 'Hello';
+var str2 = "Hello";
+console.log(typeof str1, typeof str2);
