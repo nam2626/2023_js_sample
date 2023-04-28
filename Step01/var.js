68d3fb68fdc7a39b24b016ac877a1bac9645cab2
nam2626 <nam2626@naver.com>
2023-04-28 17:14:35 +0900

    네이밍

--- Step01/var.js
+++ Step01/var.js
@@ -30,3 +30,11 @@ console.log(flag);
 //3. 변수명은 keyword(예약어)를 사용할 수 없다.
 //4. 변수명은 대소문자 구분한다.
 //5. 변수명은 공백을 포함할 수 없음.
+
+//변수 네이밍 방법
+//1. 카멜 표기법 : 첫단어는 소문자, 이후 단어는 첫글자만 대문자로 표기 --> 변수명, 함수명
+//  ex) firstName, lastName, age, isMarried
+//2. 파스칼 표기법 : 모든 단어의 첫글자를 대문자로 표기 --> 클래스명
+//  ex) FirstName, LastName, Age, IsMarried
+//3. 스네이크 표기법 : 모든 단어를 소문자로 표기하고 단어와 단어 사이를 _로 구분 --> 패키지명
+//  ex) first_name, last_name, age, is_married
