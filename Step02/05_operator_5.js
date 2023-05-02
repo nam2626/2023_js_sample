e58e957359f135e0bfe60f4d6704c122ad2598d6
nam2626 <nam2626@naver.com>
2023-05-02 15:54:10 +0900

    논리 연산자

---
+++ Step02/05_operator_5.js
@@ -0,0 +1,23 @@
+//논리 연산자
+// && , || , !
+// &&(AND) : 양쪽의 조건식이 모두 true일 때 true가 되는 연산자
+// ||(OR) : 양쪽의 조건식 중 하나라도 true 면 true가 되는 연산자
+// ! (NOT) : 조건식의 결과를 반대로 뒤집음, true -> false, false -> true
+
+// && (AND)
+let a = 10, b = 20;
+console.log(a > 5 && b < 30);
+console.log(a > 5 && b > 30);
+
+// || (OR)
+console.log(a > 5 || b < 30);
+console.log(a > 5 || b > 30);
+console.log(a < 5 || b > 30);
+
+// ! (NOT)
+console.log(!(a > 5));
+console.log(!(a > 5 || b < 30));
+console.log(!(a > 5 && b < 30));
+
+
+
