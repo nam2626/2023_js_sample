8ef16a6c5fb01f30efe87ec735ff0c0e5da5c17f
nam2626 <nam2626@naver.com>
2023-05-02 14:52:09 +0900

    복합대입연산자

---
+++ Step02/02_operator_2.js
@@ -0,0 +1,13 @@
+//복합대입 연산자
+// +=  -=  *=  /=  %=
+var a = 10;
+a += 10;//a = a + 10;
+console.log(a);
+a -= 3; 
+console.log(a);
+a *= 2; 
+console.log(a);
+a /= 4; 
+console.log(a);
+a %= 5; 
+console.log(a);
