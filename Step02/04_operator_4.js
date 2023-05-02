53b05170d03882b30fc47f12580b6538cb7303d3
nam2626 <nam2626@naver.com>
2023-05-02 15:36:47 +0900

    비교연산자

---
+++ Step02/04_operator_4.js
@@ -0,0 +1,16 @@
+// 비교 연산자
+// >  <  >=  <=  ==  !=
+let a = 10, b = 20;
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+console.log(a == b);
+console.log(a != b);
+
+let c = 10, d = '10';
+console.log(c == d);//false?
+console.log(c != d);//true?
+//타입까지 비교
+console.log(c === d);
+console.log(c !== d);
