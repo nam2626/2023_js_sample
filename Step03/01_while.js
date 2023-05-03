4551409f6f3c04fb933071aa4aca71c6c69aae8e
nam2626 <nam2626@naver.com>
2023-05-03 15:11:36 +0900

    while 문

---
+++ Step03/01_while.js
@@ -0,0 +1,16 @@
+/*
+    while문
+    - 조건식이 true 일 동안 반복
+    - 조건식이 false가 되야 반복이 종료된다.
+
+    while(조건식){
+        반복할 코드
+        ...
+    }
+*/
+//Hello World를 5번 출력
+let i = 5;
+while(i > 0){
+    console.log('Hello World',i);
+    i--;
+}
