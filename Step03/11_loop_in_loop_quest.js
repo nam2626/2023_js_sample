feb62d3f8ef04b5fe61643a051beffb724bb4085
nam2626 <nam2626@naver.com>
2023-05-03 17:50:07 +0900

    loop in loop

--- Step03/11_loop_in_loop_quest.js
+++ Step03/11_loop_in_loop_quest.js
@@ -6,3 +6,26 @@
      *******
     *********
 */
+for(i=0;i<5;i++){
+    str = '';
+    for(j=0;j<5-1-i;j++){
+        str += ' ';
+    }
+    for(j=0;j< i * 2 + 1;j++){
+        str += '*'
+    }
+    console.log(str);
+}
+
+console.log('----------------');
+for (i = 0; i < 5; i++) {
+    var star = '';
+    for (j = 0; j < 9; j++) {
+        if (j < 4 - i || j > 4 + i) {
+            star += ' ';
+        } else {
+            star += '*';
+        }
+    }
+    console.log(star);
+}
